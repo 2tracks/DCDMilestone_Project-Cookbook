2e174d2507a9b0b62b7e463db94d30370ebe7bc2
Gabriela DBS <gabrieladisalvatore@gmail.com>
2020-05-17 18:03:02 +0000

    Added card with card-title and icons to all_recipes

--- app.py
+++ app.py
@@ -16,11 +16,22 @@ app.config["MONGO_URI"] = os.getenv('MONGO_URI')
 
 mongo = PyMongo(app)
 
+recipe_info = mongo.db.recipe_info
+categories = mongo.db.categories
+skill_level = mongo.db.skill_level
+allergens = mongo.db.allergens
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html", recipe_info=mongo.db.recipe_info.find())
+
+
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("all_recipes.html",recipe_info=recipe_info.find().sort('date_time, pymongo.DESCENDING'),
+                            categories=categories.find())
 
 
 if __name__ == '__main__':
